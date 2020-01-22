7b976a799e73168c3b58f07344957228322bfa77
Nate <nateschutte@mac.com>
2020-01-22 00:35:47 -0500

    project setup (codeblocks), dlls, directories
    no .gitignore yet so the profile can be transferred

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace sf;
+
+int main() {
+
+    RenderWindow window(VideoMode(600, 400), "SFML says hello!");
+
+    CircleShape shape(100.f);
+    shape.setOrigin(10.0f, 10.0f);
+    shape.setPosition(0, 150.0f);
+    shape.setFillColor(Color::Green);
+
+    Event event;
+
+    while(window.isOpen()) {
+        while(window.pollEvent(event)) {
+            if(event.type == Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
